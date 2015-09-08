414b685938087549b171761de3ecabe65bc4ca35
Janice Dunn & Christoph Pirringer <student@epicodus.com>
2015-09-08 10:54:28 -0700

    Pass spec tests for all triangles.

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,21 @@
 describe('triangleTracker', function() {
   it("is an equilateral triangle. all sides are equal", function() {
-    expect(leapYear(3, 3, 3)).to.equal("equilateral");
+    expect(triangleTracker(3, 3, 3)).to.equal("n equilateral");
   });
 
   it("is an isoceles triangle. two of its sides are equal", function() {
-    expect(leapYear(3, 3, 5)).to.equal("isoceles");
+    expect(triangleTracker(3, 3, 5)).to.equal("n isoceles");
   });
 
   it("is an isoceles triangle. two of its sides are equal", function() {
-    expect(leapYear(5, 3, 5)).to.equal("isoceles");
+    expect(triangleTracker(5, 3, 5)).to.equal("n isoceles");
   });
 
   it("is a scalene triangle. none of its sides are equal", function() {
-    expect(leapYear(2, 3, 4)).to.equal("scalene");
+    expect(triangleTracker(2, 3, 4)).to.equal(" scalene");
+  });
+
+  it("is not a triangle if one side is the same or larger than the sum of the other two", function() {
+    expect(triangleTracker(2, 2, 8)).to.equal("n impossible");
   });
 });
